569327f59456fcc50c649b86488781f5ea53a671
adeekahh <ekydesignlab@gmail.com>
2018-12-14 12:54:26 +0100

    save before blog

--- blog.js
+++ blog.js
@@ -1,5 +1,5 @@
 // BLOG DATA
-
+/*
 const templatePosts = document.querySelector("#template-blogposts").content;
 
 function getBlogPosts() {
@@ -21,7 +21,7 @@ function showSinglePost(post) {
 	/*
   copy2.querySelector(".bodytext").textContent = post.acf.bodytext;
   copy2.querySelector(".date").textContent = post.acf.date;
-  copy2.querySelector(".author").textContent = post.acf.author;*/
+  copy2.querySelector(".author").textContent = post.acf.author;
   document.querySelector(".blog-post-container").appendChild(copy2);
 	setTimeout(document.querySelector('.featured-readmore-button').addEventListener('click', showFeaturedText), 100);
 
@@ -48,6 +48,64 @@ function hideFeaturedText(){
 
 };
 
+*/
+
+
+let page = 1;
+let lookingForData = false;
+
+function fetchBlog() {
+    lookingForData = true;
+    fetch("http://tomasgazi.com/wordpress/wp-json/wp/v2/blog?_embed&per_page=4&page=" + page)
+        .then(e => e.json())
+        .then(showBlog)
+}
+
+function showBlog(data) {
+    console.log(data);
+    lookingForData = false;
+    data.forEach(showSinglePost)
+}
+
+function showSinglePost(aPost) {
+    console.log(aPost);
+    let template = document.querySelector("#blogtemp").content;
+    let clone = template.cloneNode(true);
+    clone.querySelector("h1").innerHTML = aPost.title.rendered;
+    clone.querySelector("img").setAttribute("src", aPost._embedded["wp:featuredmedia"][0].media_details.sizes.medium_large.source_url);
+    let blogposts = document.querySelector("#blogposts");
+    clone.querySelector('.readmore').href = "subpage.html?id=" + aPost.id;
+    blogposts.appendChild(clone);
+
+}
+
+fetchBlog();
+
+setInterval(function () {
+
+    if (bottomVisible() && lookingForData === false) {
+        page++;
+        fetchBlog();
+    }
+}, 100)
+
+
+function bottomVisible() {
+    const scrollY = window.scrollY
+    const visible = document.documentElement.clientHeight
+    const pageHeight = document.documentElement.scrollHeight
+    const bottomOfPage = visible + scrollY >= pageHeight
+    return bottomOfPage || pageHeight < visible
+}
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
 
 
 
